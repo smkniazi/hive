242f3e9335a2324f3b20634c58513c1ace858cf4
Wang Haihua <win19999@gmail.com>
2017-06-10 07:25:00 -0700

    HIVE-16877 : NPE when issue query like alter table ... cascade onto non-partitioned table (Wang Haihua via Ashutosh Chauhan)
    Signed-off-by: Ashutosh Chauhan <hashutosh@apache.org>

--- ql/src/java/org/apache/hadoop/hive/ql/parse/DDLSemanticAnalyzer.java
+++ ql/src/java/org/apache/hadoop/hive/ql/parse/DDLSemanticAnalyzer.java
@@ -1507,6 +1507,12 @@ public class DDLSemanticAnalyzer extends BaseSemanticAnalyzer {
     }
 
     Table tab = getTable(tableName, true);
+    // cascade only occurs with partitioned table
+    if (isCascade && !tab.isPartitioned()) {
+      throw new SemanticException(
+          ErrorMsg.ALTER_TABLE_NON_PARTITIONED_TABLE_CASCADE_NOT_SUPPORTED);
+    }
+
     // Determine the lock type to acquire
     WriteEntity.WriteType writeType = doForceExclusive
         ? WriteType.DDL_EXCLUSIVE : WriteEntity.determineAlterTableWriteType(op);
