ebafc814eda329b040f8d237e6790002a52301b4
Eugene Koifman <ekoifman@hortonworks.com>
2017-07-18 10:25:56 -0700

    HIVE-17069 Refactor OrcRawRecrodMerger.ReaderPair (Eugene Koifman, reviewed by Alan Gates)

--- ql/src/java/org/apache/hadoop/hive/ql/exec/FileSinkOperator.java
+++ ql/src/java/org/apache/hadoop/hive/ql/exec/FileSinkOperator.java
@@ -75,6 +75,7 @@ import org.slf4j.LoggerFactory;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.Serializable;
+import java.io.StringWriter;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
