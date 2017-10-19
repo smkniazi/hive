42cbf4f2a4d7d6a9b13246cfe777724da65a23f0
Fabio Buso <dev.siroibaf@gmail.com>
2017-10-19 16:51:03 +0200

    Refactor deployment script

--- kompics-deploy.sh
+++ kompics-deploy.sh
@@ -2,6 +2,27 @@
 
 VERSION=2.3.0
 
+# Hive Common
+mvn  deploy:deploy-file -Durl=scpexe://kompics.i.sics.se/home/maven/repository \
+                      -DrepositoryId=sics-release-repository \
+                      -Dfile=./common/target/hive-common-${VERSION}.jar \
+                      -DgroupId=io.hops \
+                      -DartifactId=hive-common \
+                      -Dversion=${VERSION} \
+                      -Dpackaging=jar \
+                      -DpomFile=./common/pom.xml \
+-DgeneratePom.description="Hive Common"
+
+mvn  deploy:deploy-file -Durl=scpexe://kompics.i.sics.se/home/maven/repository \
+                      -DrepositoryId=sics-release-repository \
+                      -Dfile=./jdbc/target/hive-jdbc-${VERSION}.jar \
+                      -DgroupId=io.hops \
+                      -DartifactId=hive-jdbc \
+                      -Dversion=${VERSION} \
+                      -Dpackaging=jar \
+                      -DpomFile=./jdbc/pom.xml \
+-DgeneratePom.description="Hive JDBC"
+
 mvn  deploy:deploy-file -Durl=scpexe://kompics.i.sics.se/home/maven/repository \
                       -DrepositoryId=sics-release-repository \
                       -Dfile=./jdbc-handler/target/hive-jdbc-handler-${VERSION}.jar \
@@ -11,3 +32,23 @@ mvn  deploy:deploy-file -Durl=scpexe://kompics.i.sics.se/home/maven/repository \
                       -Dpackaging=jar \
                       -DpomFile=./jdbc-handler/pom.xml \
 -DgeneratePom.description="Hive JDBC Handler"
+
+mvn  deploy:deploy-file -Durl=scpexe://kompics.i.sics.se/home/maven/repository \
+                      -DrepositoryId=sics-release-repository \
+                      -Dfile=./service/target/hive-service-${VERSION}.jar \
+                      -DgroupId=io.hops \
+                      -DartifactId=hive-service \
+                      -Dversion=${VERSION} \
+                      -Dpackaging=jar \
+                      -DpomFile=./service/pom.xml \
+-DgeneratePom.description="Hive Service"
+
+mvn  deploy:deploy-file -Durl=scpexe://kompics.i.sics.se/home/maven/repository \
+                      -DrepositoryId=sics-release-repository \
+                      -Dfile=./service-rpc/target/hive-service-rpc-${VERSION}.jar \
+                      -DgroupId=io.hops \
+                      -DartifactId=hive-service-rpc \
+                      -Dversion=${VERSION} \
+                      -Dpackaging=jar \
+                      -DpomFile=./service-rpc/pom.xml \
+-DgeneratePom.description="Hive Service"
