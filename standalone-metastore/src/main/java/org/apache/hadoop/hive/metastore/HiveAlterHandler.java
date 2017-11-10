7af9b5221ec98fd1c0e68ed40eb24d5b88343d3c
Peter Vary <pvary@cloudera.com>
2017-11-10 11:50:16 +0100

    HIVE-17969: Metastore to alter table in batches of partitions when renaming table (Adam Szita, via Peter Vary)

--- standalone-metastore/src/main/java/org/apache/hadoop/hive/metastore/HiveAlterHandler.java
+++ standalone-metastore/src/main/java/org/apache/hadoop/hive/metastore/HiveAlterHandler.java
@@ -57,6 +57,7 @@ import java.net.URI;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
