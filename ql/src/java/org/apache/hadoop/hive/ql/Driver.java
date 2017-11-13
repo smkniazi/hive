8c3c462ef90f221a79bc725e454b173894120921
Anishek Agarwal <anishek@gmail.com>
2017-11-13 10:38:15 +0530

    HIVE-17615: Task.executeTask has to be thread safe for parallel execution (Anishek Agarwal reviewed by Daniel Dai)

--- ql/src/java/org/apache/hadoop/hive/ql/Driver.java
+++ ql/src/java/org/apache/hadoop/hive/ql/Driver.java
@@ -2293,8 +2293,7 @@ public class Driver implements CommandProcessor {
       console.printInfo("Launching Job " + cxt.getCurJobNo() + " out of " + jobs);
     }
     tsk.initialize(queryState, plan, cxt, ctx.getOpContext());
-    TaskResult tskRes = new TaskResult();
-    TaskRunner tskRun = new TaskRunner(tsk, tskRes);
+    TaskRunner tskRun = new TaskRunner(tsk);
 
     cxt.launching(tskRun);
     // Launch Task
