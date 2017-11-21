c986baf750d99a48cda25a775bf7c511c7d6384f
Zoltan Haindrich <kirk@rxd.hu>
2017-11-21 20:27:38 +0100

    HIVE-18063: Make CommandProcessorResponse an exception instead of a return class (Zoltan Haindrich, reviewed by Ashutosh Chauhan)
    Signed-off-by: Zoltan Haindrich <kirk@rxd.hu>

--- itests/hive-unit/src/test/java/org/apache/hadoop/hive/ql/TestAcidOnTez.java
+++ itests/hive-unit/src/test/java/org/apache/hadoop/hive/ql/TestAcidOnTez.java
@@ -139,8 +139,8 @@ public class TestAcidOnTez {
     try {
       if (d != null) {
         dropTables();
-        d.destroy();
         d.close();
+        d.destroy();
         d = null;
       }
       TxnDbUtil.cleanDb(hiveConf);
@@ -776,8 +776,8 @@ ekoifman:apache-hive-3.0.0-SNAPSHOT-bin ekoifman$ tree  ~/dev/hiverwgit/itests/h
       ss.close();
     }
     if (d != null) {
-      d.destroy();
       d.close();
+      d.destroy();
     }
 
     SessionState.start(conf);
