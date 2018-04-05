5720fe1345a9c04ef7766d3aa8f577ada8b69057
Alan Gates <gates@hortonworks.com>
2018-04-05 16:55:55 -0700

    HIVE-19031 Mark duplicate configs in HiveConf as deprecated (Alan Gates, reviewed by Thejas Nair)

--- standalone-metastore/src/main/java/org/apache/hadoop/hive/metastore/RawStore.java
+++ standalone-metastore/src/main/java/org/apache/hadoop/hive/metastore/RawStore.java
@@ -1166,6 +1166,17 @@ public interface RawStore extends Configurable {
     List<String> partNames, List<String> colNames) throws MetaException, NoSuchObjectException;
 
   /**
+   * Get column stats for all partitions of all tables in the database
+   * @param catName catalog name
+   * @param dbName database name
+   * @return List of column stats objects for all partitions of all tables in the database
+   * @throws MetaException error accessing RDBMS
+   * @throws NoSuchObjectException no such database
+   */
+  List<ColStatsObjWithSourceInfo> getPartitionColStatsForDatabase(String catName, String dbName)
+      throws MetaException, NoSuchObjectException;
+
+  /**
    * Get the next notification event.
    * @param rqst Request containing information on the last processed notification.
    * @return list of notifications, sorted by eventId
