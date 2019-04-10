difin/difin_hive

HIVE-18179 : Implement validate resource plan (part 1) (Harish Jaiprakash, reviewed by Sergey Shelukhin)

standalone-metastore/src/gen/thrift/gen-php/metastore/Types.php
@@ -22300,22 +22300,26 @@ class WMValidateResourcePlanResponse {
   static $_TSPEC;
 
   /**
-   * @var bool
+   * @var string[]
    */
-  public $isValid = null;
+  public $errors = null;
 
   public function __construct($vals=null) {
     if (!isset(self::$_TSPEC)) {
       self::$_TSPEC = array(
         1 => array(
-          'var' => 'isValid',
-          'type' => TType::BOOL,
+          'var' => 'errors',
+          'type' => TType::LST,
+          'etype' => TType::STRING,
+          'elem' => array(
+            'type' => TType::STRING,
+            ),
           ),
         );
     }
     if (is_array($vals)) {
-      if (isset($vals['isValid'])) {
-        $this->isValid = $vals['isValid'];
+      if (isset($vals['errors'])) {
+        $this->errors = $vals['errors'];
       }
     }
   }
@@ -22340,8 +22344,18 @@ class WMValidateResourcePlanResponse {
       switch ($fid)
       {
         case 1:
-          if ($ftype == TType::BOOL) {
-            $xfer += $input->readBool($this->isValid);
+          if ($ftype == TType::LST) {
+            $this->errors = array();
+            $_size687 = 0;
+            $_etype690 = 0;
+            $xfer += $input->readListBegin($_etype690, $_size687);
+            for ($_i691 = 0; $_i691 < $_size687; ++$_i691)
+            {
+              $elem692 = null;
+              $xfer += $input->readString($elem692);
+              $this->errors []= $elem692;
+            }
+            $xfer += $input->readListEnd();
           } else {
             $xfer += $input->skip($ftype);
           }
@@ -22359,9 +22373,21 @@ class WMValidateResourcePlanResponse {
   public function write($output) {
     $xfer = 0;
     $xfer += $output->writeStructBegin('WMValidateResourcePlanResponse');
-    if ($this->isValid !== null) {
-      $xfer += $output->writeFieldBegin('isValid', TType::BOOL, 1);
-      $xfer += $output->writeBool($this->isValid);
+    if ($this->errors !== null) {
+      if (!is_array($this->errors)) {
+        throw new TProtocolException('Bad type in structure.', TProtocolException::INVALID_DATA);
+      }
+      $xfer += $output->writeFieldBegin('errors', TType::LST, 1);
+      {
+        $output->writeListBegin(TType::STRING, count($this->errors));
+        {
+          foreach ($this->errors as $iter693)
+          {
+            $xfer += $output->writeString($iter693);
+          }
+        }
+        $output->writeListEnd();
+      }
       $xfer += $output->writeFieldEnd();
     }
     $xfer += $output->writeFieldStop();
@@ -23030,15 +23056,15 @@ class WMGetTriggersForResourePlanResponse {
         case 1:
           if ($ftype == TType::LST) {
             $this->triggers = array();
-            $_size687 = 0;
-            $_etype690 = 0;
-            $xfer += $input->readListBegin($_etype690, $_size687);
-            for ($_i691 = 0; $_i691 < $_size687; ++$_i691)
+            $_size694 = 0;
+            $_etype697 = 0;
+            $xfer += $input->readListBegin($_etype697, $_size694);
+            for ($_i698 = 0; $_i698 < $_size694; ++$_i698)
             {
-              $elem692 = null;
-              $elem692 = new \metastore\WMTrigger();
-              $xfer += $elem692->read($input);
-              $this->triggers []= $elem692;
+              $elem699 = null;
+              $elem699 = new \metastore\WMTrigger();
+              $xfer += $elem699->read($input);
+              $this->triggers []= $elem699;
             }
             $xfer += $input->readListEnd();
           } else {
@@ -23066,9 +23092,9 @@ class WMGetTriggersForResourePlanResponse {
       {
         $output->writeListBegin(TType::STRUCT, count($this->triggers));
         {
-          foreach ($this->triggers as $iter693)
+          foreach ($this->triggers as $iter700)
           {
-            $xfer += $iter693->write($output);
+            $xfer += $iter700->write($output);
           }
         }
         $output->writeListEnd();

smkniazi/hive

standalone-metastore/src/gen/thrift/gen-php/metastore/Types.php
@@ -23129,22 +23129,26 @@ class WMValidateResourcePlanResponse {
   static $_TSPEC;
 
   /**
-   * @var bool
+   * @var string[]
    */
-  public $isValid = null;
+  public $errors = null;
 
   public function __construct($vals=null) {
     if (!isset(self::$_TSPEC)) {
       self::$_TSPEC = array(
         1 => array(
-          'var' => 'isValid',
-          'type' => TType::BOOL,
+          'var' => 'errors',
+          'type' => TType::LST,
+          'etype' => TType::STRING,
+          'elem' => array(
+            'type' => TType::STRING,
+            ),
           ),
         );
     }
     if (is_array($vals)) {
-      if (isset($vals['isValid'])) {
-        $this->isValid = $vals['isValid'];
+      if (isset($vals['errors'])) {
+        $this->errors = $vals['errors'];
       }
     }
   }
@@ -23169,8 +23173,18 @@ class WMValidateResourcePlanResponse {
       switch ($fid)
       {
         case 1:
-          if ($ftype == TType::BOOL) {
-            $xfer += $input->readBool($this->isValid);
+          if ($ftype == TType::LST) {
+            $this->errors = array();
+            $_size694 = 0;
+            $_etype697 = 0;
+            $xfer += $input->readListBegin($_etype697, $_size694);
+            for ($_i698 = 0; $_i698 < $_size694; ++$_i698)
+            {
+              $elem699 = null;
+              $xfer += $input->readString($elem699);
+              $this->errors []= $elem699;
+            }
+            $xfer += $input->readListEnd();
           } else {
             $xfer += $input->skip($ftype);
           }
@@ -23188,9 +23202,21 @@ class WMValidateResourcePlanResponse {
   public function write($output) {
     $xfer = 0;
     $xfer += $output->writeStructBegin('WMValidateResourcePlanResponse');
-    if ($this->isValid !== null) {
-      $xfer += $output->writeFieldBegin('isValid', TType::BOOL, 1);
-      $xfer += $output->writeBool($this->isValid);
+    if ($this->errors !== null) {
+      if (!is_array($this->errors)) {
+        throw new TProtocolException('Bad type in structure.', TProtocolException::INVALID_DATA);
+      }
+      $xfer += $output->writeFieldBegin('errors', TType::LST, 1);
+      {
+        $output->writeListBegin(TType::STRING, count($this->errors));
+        {
+          foreach ($this->errors as $iter700)
+          {
+            $xfer += $output->writeString($iter700);
+          }
+        }
+        $output->writeListEnd();
+      }
       $xfer += $output->writeFieldEnd();
     }
     $xfer += $output->writeFieldStop();
@@ -23859,15 +23885,15 @@ class WMGetTriggersForResourePlanResponse {
         case 1:
           if ($ftype == TType::LST) {
             $this->triggers = array();
-            $_size694 = 0;
-            $_etype697 = 0;
-            $xfer += $input->readListBegin($_etype697, $_size694);
-            for ($_i698 = 0; $_i698 < $_size694; ++$_i698)
+            $_size701 = 0;
+            $_etype704 = 0;
+            $xfer += $input->readListBegin($_etype704, $_size701);
+            for ($_i705 = 0; $_i705 < $_size701; ++$_i705)
             {
-              $elem699 = null;
-              $elem699 = new \metastore\WMTrigger();
-              $xfer += $elem699->read($input);
-              $this->triggers []= $elem699;
+              $elem706 = null;
+              $elem706 = new \metastore\WMTrigger();
+              $xfer += $elem706->read($input);
+              $this->triggers []= $elem706;
             }
             $xfer += $input->readListEnd();
           } else {
@@ -23895,9 +23921,9 @@ class WMGetTriggersForResourePlanResponse {
       {
         $output->writeListBegin(TType::STRUCT, count($this->triggers));
         {
-          foreach ($this->triggers as $iter700)
+          foreach ($this->triggers as $iter707)
           {
-            $xfer += $iter700->write($output);
+            $xfer += $iter707->write($output);
           }
         }
         $output->writeListEnd();