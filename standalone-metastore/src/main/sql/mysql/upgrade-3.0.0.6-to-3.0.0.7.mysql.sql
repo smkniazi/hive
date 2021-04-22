0e5b5ffbcf6946a74465ac5ba06ca56944edffca
Fabio Buso <buso.fabio@gmail.com>
2021-04-22 18:20:00 +0200

    Release version 3.0.0.7

---
+++ standalone-metastore/src/main/sql/mysql/upgrade-3.0.0.6-to-3.0.0.7.mysql.sql
@@ -0,0 +1,6 @@
+SELECT 'Upgrading MetaStore schema from 3.0.0.6 to 3.0.0.7' AS ' ';
+
+
+-- These lines need to be last.  Insert any changes above.
+UPDATE VERSION SET SCHEMA_VERSION='3.0.0.7', VERSION_COMMENT='Hive release version 3.0.0.7' where VER_ID=1;
+SELECT 'Finished upgrading MetaStore schema from 3.0.0.6 to 3.0.0.7' AS ' ';
